bd3f60a78ba79a80b167cf702742d855b5318fc9
YoungHwan Joo <email@rulyox.com>
2020-02-10 21:44:45 +0900

    Initial commit

---
+++ app/src/main/java/com/rulyox/linechallengememo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.rulyox.linechallengememo
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+    }
+
+}
